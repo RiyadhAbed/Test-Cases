c56052489f5f4eee9f42efa43ba93f1dc62ec77d
RiyadhAbed <riyadh.abed@tekschool.us>
2022-05-10 10:32:33 -0400

    change after add

--- src/GitHubPractic1/Git_HuBTestCase.java
+++ src/GitHubPractic1/Git_HuBTestCase.java
@@ -3,7 +3,10 @@ package GitHubPractic1;
 public class Git_HuBTestCase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	  int a = 20;
+	  int c = 10;
+	  int d = a + c ;
+	  System.out.println(d);
 
 	}
 
