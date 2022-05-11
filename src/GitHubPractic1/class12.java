3834d4dc18f6b601a1d192dbc074623cb6d27076
RiyadhAbed <riyadh.abed@tekschool.us>
2022-05-10 20:05:39 -0400

    Class 12

---
+++ src/GitHubPractic1/class12.java
@@ -0,0 +1,15 @@
+package GitHubPractic1;
+
+public class class12 {
+
+	public static void main(String[] args) {
+    
+		int A = 90;
+		int B = 10;
+		int c = A-B;
+		System.out.println(c);
+		
+		
+	}
+
+}
